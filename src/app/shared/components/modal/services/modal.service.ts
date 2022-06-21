f8367f971e2880fb8af81905396d53396fd5fa7e
DionatanLeao <dionatan.ti@outlook.com>
2022-06-21 14:59:45 -0300

    ComponentFactoryResolver

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,22 +1,33 @@
-import { Injectable, TemplateRef } from '@angular/core';
+import { ComponentFactory, ComponentFactoryResolver, Injectable, Injector, TemplateRef } from '@angular/core';
 import { ModalConfig } from '../interfaces/model-config';
+import { ModalComponent } from '../modal/modal.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() { }
+  private componentFactory: ComponentFactory<ModalComponent>
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector
+  ) {
+    this.componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
 
   public open(config: ModalConfig): ModalRef {
+    const componentRef = this.componentFactory.create(this.injector);
     console.log('open called');
     return new ModalRef();
   }
+
 }
 
 export class ModalRef {
-  
+
   public close(): void {
     console.log('close called')
   }
+
 }
